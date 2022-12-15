f6a7e6445c4c84ae8d68598c56231c69222667b8
zhengtian <mazt1025@hotmail.com>
2022-12-15 15:55:15 +0800

    Publish empty map if obs_num is set to 0

--- src/random_forest_sensing.cpp
+++ src/random_forest_sensing.cpp
@@ -64,236 +64,260 @@ pcl::PointCloud<pcl::PointXYZ> clicked_cloud_;
 
 void RandomMapGenerate()
 {
-  pcl::PointXYZ pt_random;
+  bool empty_map = false;
 
-  rand_x = uniform_real_distribution<double>(_x_l, _x_h);
-  rand_y = uniform_real_distribution<double>(_y_l, _y_h);
-  rand_w = uniform_real_distribution<double>(_w_l, _w_h);
-  rand_h = uniform_real_distribution<double>(_h_l, _h_h);
-
-  rand_radius_ = uniform_real_distribution<double>(radius_l_, radius_h_);
-  rand_radius2_ = uniform_real_distribution<double>(radius_l_, 1.2);
-  rand_theta_ = uniform_real_distribution<double>(-theta_, theta_);
-  rand_z_ = uniform_real_distribution<double>(z_l_, z_h_);
-
-  // generate polar obs
-  for (int i = 0; i < _obs_num; i++)
+  if (_obs_num < 1)
   {
-    double x, y, w, h;
-    x = rand_x(eng);
-    y = rand_y(eng);
-    w = rand_w(eng);
-
-    x = floor(x / _resolution) * _resolution + _resolution / 2.0;
-    y = floor(y / _resolution) * _resolution + _resolution / 2.0;
-
-    int widNum = ceil(w / _resolution);
-
-    for (int r = -widNum / 2.0; r < widNum / 2.0; r++)
-      for (int s = -widNum / 2.0; s < widNum / 2.0; s++)
-      {
-        h = rand_h(eng);
-        int heiNum = ceil(h / _resolution);
-        for (int t = -20; t < heiNum; t++)
-        {
-          pt_random.x = x + (r + 0.5) * _resolution + 1e-2;
-          pt_random.y = y + (s + 0.5) * _resolution + 1e-2;
-          pt_random.z = (t + 0.5) * _resolution + 1e-2;
-          cloudMap.points.push_back(pt_random);
-        }
-      }
+    cloudMap.points.clear();
+    empty_map = true;
   }
 
-  // generate circle obs
-  for (int i = 0; i < circle_num_; ++i)
+  else
   {
-    double x, y, z;
-    x = rand_x(eng);
-    y = rand_y(eng);
-    z = rand_z_(eng);
+    pcl::PointXYZ pt_random;
 
-    x = floor(x / _resolution) * _resolution + _resolution / 2.0;
-    y = floor(y / _resolution) * _resolution + _resolution / 2.0;
-    z = floor(z / _resolution) * _resolution + _resolution / 2.0;
+    rand_x = uniform_real_distribution<double>(_x_l, _x_h);
+    rand_y = uniform_real_distribution<double>(_y_l, _y_h);
+    rand_w = uniform_real_distribution<double>(_w_l, _w_h);
+    rand_h = uniform_real_distribution<double>(_h_l, _h_h);
 
-    Eigen::Vector3d translate(x, y, z);
+    rand_radius_ = uniform_real_distribution<double>(radius_l_, radius_h_);
+    rand_radius2_ = uniform_real_distribution<double>(radius_l_, 1.2);
+    rand_theta_ = uniform_real_distribution<double>(-theta_, theta_);
+    rand_z_ = uniform_real_distribution<double>(z_l_, z_h_);
 
-    double theta = rand_theta_(eng);
-    Eigen::Matrix3d rotate;
-    rotate << cos(theta), -sin(theta), 0.0, sin(theta), cos(theta), 0.0, 0, 0,
-        1;
-
-    double radius1 = rand_radius_(eng);
-    double radius2 = rand_radius2_(eng);
-
-    // draw a circle centered at (x,y,z)
-    Eigen::Vector3d cpt;
-    for (double angle = 0.0; angle < 6.282; angle += _resolution / 2)
+    // generate polar obs
+    for (int i = 0; i < _obs_num; i++)
     {
-      cpt(0) = 0.0;
-      cpt(1) = radius1 * cos(angle);
-      cpt(2) = radius2 * sin(angle);
+      double x, y, w, h;
+      x = rand_x(eng);
+      y = rand_y(eng);
+      w = rand_w(eng);
 
-      // inflate
-      Eigen::Vector3d cpt_if;
-      for (int ifx = -0; ifx <= 0; ++ifx)
-        for (int ify = -0; ify <= 0; ++ify)
-          for (int ifz = -0; ifz <= 0; ++ifz)
+      x = floor(x / _resolution) * _resolution + _resolution / 2.0;
+      y = floor(y / _resolution) * _resolution + _resolution / 2.0;
+
+      int widNum = ceil(w / _resolution);
+
+      for (int r = -widNum / 2.0; r < widNum / 2.0; r++)
+        for (int s = -widNum / 2.0; s < widNum / 2.0; s++)
+        {
+          h = rand_h(eng);
+          int heiNum = ceil(h / _resolution);
+          for (int t = -20; t < heiNum; t++)
           {
-            cpt_if = cpt + Eigen::Vector3d(ifx * _resolution, ify * _resolution,
-                                           ifz * _resolution);
-            cpt_if = rotate * cpt_if + Eigen::Vector3d(x, y, z);
-            pt_random.x = cpt_if(0);
-            pt_random.y = cpt_if(1);
-            pt_random.z = cpt_if(2);
-            cloudMap.push_back(pt_random);
+            pt_random.x = x + (r + 0.5) * _resolution + 1e-2;
+            pt_random.y = y + (s + 0.5) * _resolution + 1e-2;
+            pt_random.z = (t + 0.5) * _resolution + 1e-2;
+            cloudMap.points.push_back(pt_random);
           }
+        }
+    }
+
+    // generate circle obs
+    for (int i = 0; i < circle_num_; ++i)
+    {
+      double x, y, z;
+      x = rand_x(eng);
+      y = rand_y(eng);
+      z = rand_z_(eng);
+
+      x = floor(x / _resolution) * _resolution + _resolution / 2.0;
+      y = floor(y / _resolution) * _resolution + _resolution / 2.0;
+      z = floor(z / _resolution) * _resolution + _resolution / 2.0;
+
+      Eigen::Vector3d translate(x, y, z);
+
+      double theta = rand_theta_(eng);
+      Eigen::Matrix3d rotate;
+      rotate << cos(theta), -sin(theta), 0.0, sin(theta), cos(theta), 0.0, 0, 0,
+          1;
+
+      double radius1 = rand_radius_(eng);
+      double radius2 = rand_radius2_(eng);
+
+      // draw a circle centered at (x,y,z)
+      Eigen::Vector3d cpt;
+      for (double angle = 0.0; angle < 6.282; angle += _resolution / 2)
+      {
+        cpt(0) = 0.0;
+        cpt(1) = radius1 * cos(angle);
+        cpt(2) = radius2 * sin(angle);
+
+        // inflate
+        Eigen::Vector3d cpt_if;
+        for (int ifx = -0; ifx <= 0; ++ifx)
+          for (int ify = -0; ify <= 0; ++ify)
+            for (int ifz = -0; ifz <= 0; ++ifz)
+            {
+              cpt_if = cpt + Eigen::Vector3d(ifx * _resolution, ify * _resolution,
+                                             ifz * _resolution);
+              cpt_if = rotate * cpt_if + Eigen::Vector3d(x, y, z);
+              pt_random.x = cpt_if(0);
+              pt_random.y = cpt_if(1);
+              pt_random.z = cpt_if(2);
+              cloudMap.push_back(pt_random);
+            }
+      }
     }
   }
 
   cloudMap.width = cloudMap.points.size();
   cloudMap.height = 1;
   cloudMap.is_dense = true;
-
-  ROS_WARN("Finished generate random map ");
-
-  kdtreeLocalMap.setInputCloud(cloudMap.makeShared());
-
   _map_ok = true;
+
+  if (empty_map)
+  {
+    ROS_WARN("Finished generate empty map ");
+  }
+
+  else
+  {
+    ROS_WARN("Finished generate random map ");
+    kdtreeLocalMap.setInputCloud(cloudMap.makeShared());
+  }
 }
 
 void RandomMapGenerateCylinder()
 {
-  pcl::PointXYZ pt_random;
+  bool empty_map = false;
 
-  vector<Eigen::Vector2d> obs_position;
-
-  rand_x = uniform_real_distribution<double>(_x_l, _x_h);
-  rand_y = uniform_real_distribution<double>(_y_l, _y_h);
-  rand_w = uniform_real_distribution<double>(_w_l, _w_h);
-  rand_h = uniform_real_distribution<double>(_h_l, _h_h);
-  rand_inf = uniform_real_distribution<double>(0.5, 1.5);
-
-  rand_radius_ = uniform_real_distribution<double>(radius_l_, radius_h_);
-  rand_radius2_ = uniform_real_distribution<double>(radius_l_, 1.2);
-  rand_theta_ = uniform_real_distribution<double>(-theta_, theta_);
-  rand_z_ = uniform_real_distribution<double>(z_l_, z_h_);
-
-  // generate polar obs
-  for (int i = 0; i < _obs_num && ros::ok(); i++)
+  if (_obs_num < 1)
   {
-    double x, y, w, h, inf;
-    x = rand_x(eng);
-    y = rand_y(eng);
-    w = rand_w(eng);
-    inf = rand_inf(eng);
+    cloudMap.points.clear();
+    empty_map = true;
+  }
 
-    bool flag_continue = false;
-    for (auto p : obs_position)
-      if ((Eigen::Vector2d(x, y) - p).norm() < _min_dist /*metres*/)
-      {
-        i--;
-        flag_continue = true;
-        break;
-      }
-    if (flag_continue)
-      continue;
+  else
+  {
+    pcl::PointXYZ pt_random;
 
-    obs_position.push_back(Eigen::Vector2d(x, y));
+    vector<Eigen::Vector2d> obs_position;
 
-    x = floor(x / _resolution) * _resolution + _resolution / 2.0;
-    y = floor(y / _resolution) * _resolution + _resolution / 2.0;
+    rand_x = uniform_real_distribution<double>(_x_l, _x_h);
+    rand_y = uniform_real_distribution<double>(_y_l, _y_h);
+    rand_w = uniform_real_distribution<double>(_w_l, _w_h);
+    rand_h = uniform_real_distribution<double>(_h_l, _h_h);
+    rand_inf = uniform_real_distribution<double>(0.5, 1.5);
 
-    int widNum = ceil((w * inf) / _resolution);
-    double radius = (w * inf) / 2;
+    rand_radius_ = uniform_real_distribution<double>(radius_l_, radius_h_);
+    rand_radius2_ = uniform_real_distribution<double>(radius_l_, 1.2);
+    rand_theta_ = uniform_real_distribution<double>(-theta_, theta_);
+    rand_z_ = uniform_real_distribution<double>(z_l_, z_h_);
 
-    for (int r = -widNum / 2.0; r < widNum / 2.0; r++)
-      for (int s = -widNum / 2.0; s < widNum / 2.0; s++)
-      {
-        h = rand_h(eng);
-        int heiNum = ceil(h / _resolution);
-        for (int t = -10; t < heiNum; t++)
+    // generate polar obs
+    for (int i = 0; i < _obs_num && ros::ok(); i++)
+    {
+      double x, y, w, h, inf;
+      x = rand_x(eng);
+      y = rand_y(eng);
+      w = rand_w(eng);
+      inf = rand_inf(eng);
+
+      bool flag_continue = false;
+      for (auto p : obs_position)
+        if ((Eigen::Vector2d(x, y) - p).norm() < _min_dist /*metres*/)
         {
-          double temp_x = x + (r + 0.5) * _resolution + 1e-2;
-          double temp_y = y + (s + 0.5) * _resolution + 1e-2;
-          double temp_z = (t + 0.5) * _resolution + 1e-2;
-          if ((Eigen::Vector2d(temp_x, temp_y) - Eigen::Vector2d(x, y)).norm() <= radius)
+          i--;
+          flag_continue = true;
+          break;
+        }
+      if (flag_continue)
+        continue;
+
+      obs_position.push_back(Eigen::Vector2d(x, y));
+
+      x = floor(x / _resolution) * _resolution + _resolution / 2.0;
+      y = floor(y / _resolution) * _resolution + _resolution / 2.0;
+
+      int widNum = ceil((w * inf) / _resolution);
+      double radius = (w * inf) / 2;
+
+      for (int r = -widNum / 2.0; r < widNum / 2.0; r++)
+        for (int s = -widNum / 2.0; s < widNum / 2.0; s++)
+        {
+          h = rand_h(eng);
+          int heiNum = ceil(h / _resolution);
+          for (int t = -10; t < heiNum; t++)
           {
-            pt_random.x = temp_x;
-            pt_random.y = temp_y;
-            pt_random.z = temp_z;
-            cloudMap.points.push_back(pt_random);
+            double temp_x = x + (r + 0.5) * _resolution + 1e-2;
+            double temp_y = y + (s + 0.5) * _resolution + 1e-2;
+            double temp_z = (t + 0.5) * _resolution + 1e-2;
+            if ((Eigen::Vector2d(temp_x, temp_y) - Eigen::Vector2d(x, y)).norm() <= radius)
+            {
+              pt_random.x = temp_x;
+              pt_random.y = temp_y;
+              pt_random.z = temp_z;
+              cloudMap.points.push_back(pt_random);
+            }
           }
         }
-      }
-  }
+    }
 
-  // generate circle obs
-  for (int i = 0; i < circle_num_; ++i)
-  {
-    double x, y, z;
-    x = rand_x(eng);
-    y = rand_y(eng);
-    z = rand_z_(eng);
-
-    x = floor(x / _resolution) * _resolution + _resolution / 2.0;
-    y = floor(y / _resolution) * _resolution + _resolution / 2.0;
-    z = floor(z / _resolution) * _resolution + _resolution / 2.0;
-
-    Eigen::Vector3d translate(x, y, z);
-
-    double theta = rand_theta_(eng);
-    Eigen::Matrix3d rotate;
-    rotate << cos(theta), -sin(theta), 0.0, sin(theta), cos(theta), 0.0, 0, 0,
-        1;
-
-    double radius1 = rand_radius_(eng);
-    double radius2 = rand_radius2_(eng);
-
-    // draw a circle centered at (x,y,z)
-    Eigen::Vector3d cpt;
-    for (double angle = 0.0; angle < 6.282; angle += _resolution / 2)
+    // generate circle obs
+    for (int i = 0; i < circle_num_; ++i)
     {
-      cpt(0) = 0.0;
-      cpt(1) = radius1 * cos(angle);
-      cpt(2) = radius2 * sin(angle);
+      double x, y, z;
+      x = rand_x(eng);
+      y = rand_y(eng);
+      z = rand_z_(eng);
 
-      // inflate
-      Eigen::Vector3d cpt_if;
-      for (int ifx = -0; ifx <= 0; ++ifx)
-        for (int ify = -0; ify <= 0; ++ify)
-          for (int ifz = -0; ifz <= 0; ++ifz)
-          {
-            cpt_if = cpt + Eigen::Vector3d(ifx * _resolution, ify * _resolution,
-                                           ifz * _resolution);
-            cpt_if = rotate * cpt_if + Eigen::Vector3d(x, y, z);
-            pt_random.x = cpt_if(0);
-            pt_random.y = cpt_if(1);
-            pt_random.z = cpt_if(2);
-            cloudMap.push_back(pt_random);
-          }
+      x = floor(x / _resolution) * _resolution + _resolution / 2.0;
+      y = floor(y / _resolution) * _resolution + _resolution / 2.0;
+      z = floor(z / _resolution) * _resolution + _resolution / 2.0;
+
+      Eigen::Vector3d translate(x, y, z);
+
+      double theta = rand_theta_(eng);
+      Eigen::Matrix3d rotate;
+      rotate << cos(theta), -sin(theta), 0.0, sin(theta), cos(theta), 0.0, 0, 0,
+          1;
+
+      double radius1 = rand_radius_(eng);
+      double radius2 = rand_radius2_(eng);
+
+      // draw a circle centered at (x,y,z)
+      Eigen::Vector3d cpt;
+      for (double angle = 0.0; angle < 6.282; angle += _resolution / 2)
+      {
+        cpt(0) = 0.0;
+        cpt(1) = radius1 * cos(angle);
+        cpt(2) = radius2 * sin(angle);
+
+        // inflate
+        Eigen::Vector3d cpt_if;
+        for (int ifx = -0; ifx <= 0; ++ifx)
+          for (int ify = -0; ify <= 0; ++ify)
+            for (int ifz = -0; ifz <= 0; ++ifz)
+            {
+              cpt_if = cpt + Eigen::Vector3d(ifx * _resolution, ify * _resolution,
+                                             ifz * _resolution);
+              cpt_if = rotate * cpt_if + Eigen::Vector3d(x, y, z);
+              pt_random.x = cpt_if(0);
+              pt_random.y = cpt_if(1);
+              pt_random.z = cpt_if(2);
+              cloudMap.push_back(pt_random);
+            }
+      }
     }
   }
-
-  // for (double x = -1.5; x <= 1.5; x += 0.1)
-  //   for (double y = -1.5; y <= 1.5; y += 0.1)
-  //     for (double z = -0.5; z <= 3.0; z += 0.1)
-  //     {
-  //       pt_random.x = x;
-  //       pt_random.y = y;
-  //       pt_random.z = z;
-  //       cloudMap.push_back(pt_random);
-  //     }
 
   cloudMap.width = cloudMap.points.size();
   cloudMap.height = 1;
   cloudMap.is_dense = true;
-
-  ROS_WARN("Finished generate random map ");
-
-  kdtreeLocalMap.setInputCloud(cloudMap.makeShared());
-
   _map_ok = true;
+
+  if (empty_map)
+  {
+    ROS_WARN("Finished generate empty map ");
+  }
+
+  else
+  {
+    ROS_WARN("Finished generate random map ");
+    kdtreeLocalMap.setInputCloud(cloudMap.makeShared());
+  }
 }
 
 void RandomInclinedColumn()
@@ -342,7 +366,7 @@ void RandomInclinedColumn()
 
     int widNum = ceil((w * inf) / _resolution);
     // double radius = (w * inf) / 2;
-    Eigen::AngleAxisd R_v(rand_theta_tilt, Eigen::Vector3d(cos(rand_theta),sin(rand_theta),0));
+    Eigen::AngleAxisd R_v(rand_theta_tilt, Eigen::Vector3d(cos(rand_theta), sin(rand_theta), 0));
     Eigen::Matrix3d R_m = R_v.matrix();
 
     for (int r = -widNum / 2.0; r < widNum / 2.0; r++)
@@ -352,8 +376,8 @@ void RandomInclinedColumn()
         for (int t = -10; t < heiNum; t++)
         {
           Eigen::Vector3d temp_p(x + (r + 0.5) * _resolution + 1e-2, y + (s + 0.5) * _resolution + 1e-2, (t + 0.5) * _resolution + 1e-2);
-          temp_p = R_m * (temp_p - Eigen::Vector3d(x,y,0)) + Eigen::Vector3d(x,y,0);
-          
+          temp_p = R_m * (temp_p - Eigen::Vector3d(x, y, 0)) + Eigen::Vector3d(x, y, 0);
+
           pt_random.x = temp_p(0);
           pt_random.y = temp_p(1);
           pt_random.z = temp_p(2);
